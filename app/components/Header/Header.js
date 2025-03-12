39edd637738b07013ba96ecdc8276894bdafb9fc
gobinddhanjal12 <g40294431@gmail.com>
2025-03-12 15:42:52 +0530

    fixed header

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import "./Header.css";
 import Link from "next/link";
 import Image from "next/image";
+import { Menu, X } from "lucide-react";
 
 const Header = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -16,17 +17,23 @@ const Header = () => {
                 </div>
 
                 <button className="hamburger" onClick={() => setIsOpen(true)}>
-                    ☰
+                    <Menu size={36} />
                 </button>
 
                 <div className={`menu-overlay ${isOpen ? "open" : ""}`}>
-                    <button className="close-btn" onClick={() => setIsOpen(false)}>✖</button>
+
+                    <button className="close-btn" onClick={() => setIsOpen(false)}>
+                        <X size={36} />
+                    </button>
                     <nav className="nav-menu">
                         <ul>
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#">Appointments</a></li>
-                            <li><a href="#">Health Blog</a></li>
-                            <li><a href="#">Reviews</a></li>
+                            <li><Link href="/">Home</Link></li>
+                            <li><Link href="/appointments">Appointments</Link></li>
+                            <li><Link href="/services">Health Blog</Link></li>
+                            <li><Link href="/contact">Reviews</Link></li>
+                            <li><button className="login-btn btn">Login</button></li>
+                            <li><button className="register-btn btn">Register</button></li>
+
                         </ul>
                     </nav>
                 </div>
