79262177c8a8a42a6828bf79b89d37b802bfb5c4
gobinddhanjal12 <g40294431@gmail.com>
2025-04-04 10:30:40 +0530

    Added the Suspense in components

--- app/appointments/page.js
+++ app/appointments/page.js
@@ -10,16 +10,16 @@ export default function AvailableDoctors() {
   const [filters, setFilters] = useState({});
 
   return (
-    <div>
-      <div className={styles.search}>
-        <h1 className={styles.title}>Find a doctor at your own ease</h1>
-        <SearchBar setFilters={setFilters} />
-      </div>
-      <div className={styles.container}>
-        <Suspense fallback={<p>Loading...</p>}>
+    <Suspense fallback={<p>Loading...</p>}>
+      <div>
+        <div className={styles.search}>
+          <h1 className={styles.title}>Find a doctor at your own ease</h1>
+          <SearchBar setFilters={setFilters} />
+        </div>
+        <div className={styles.container}>
           <DoctorList filters={filters} />
-        </Suspense>
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
