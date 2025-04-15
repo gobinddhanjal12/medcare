05903b997d1318581963a59998b67e8d0c1f483b
gobinddhanjal12 <g40294431@gmail.com>
2025-04-15 16:03:06 +0530

    added the animation to doctor card

--- app/components/Card/DoctorCard.js
+++ app/components/Card/DoctorCard.js
@@ -44,41 +44,43 @@ const DoctorCard = ({ doctor }) => {
   };
 
   return (
-    <div className={styles.card} onClick={handleCardClick}>
-      <Image
-        width={200}
-        height={200}
-        src={photo_path}
-        alt={name}
-        className={styles.profileImage}
-      />
-      <h3 className={styles.name}>{name}</h3>
-      <div className={styles.details}>
-        <span className={styles.detail}>
-          <Stethoscope className={styles.icon} /> {specialty}
-        </span>
-        <span className={styles.detail}>
-          <Clock className={styles.icon} /> {experience} Years
-        </span>
-      </div>
-
-      <div className={styles.cardBottom}>
-        <div className={styles.ratings}>
-          <span>Ratings: </span>
-          {Array.from({ length: filledStars }, (_, i) => (
-            <Star key={i} className={styles.star} />
-          ))}
-          {Array.from({ length: emptyStars }, (_, i) => (
-            <Star key={`empty-${i}`} className={styles.emptyStar} />
-          ))}
+    <div className={styles.card} >
+      <div className={styles.content} onClick={handleCardClick}>
+        <Image
+          width={200}
+          height={200}
+          src={photo_path}
+          alt={name}
+          className={styles.profileImage}
+        />
+        <h3 className={styles.name}>{name}</h3>
+        <div className={styles.details}>
+          <span className={styles.detail}>
+            <Stethoscope className={styles.icon} /> {specialty}
+          </span>
+          <span className={styles.detail}>
+            <Clock className={styles.icon} /> {experience} Years
+          </span>
         </div>
-        <button
-          className={styles.bookButton}
-          onClick={handleBookAppointment}
-          aria-label="Book Appointment"
-        >
-          Book Appointment
-        </button>
+
+        <div className={styles.cardBottom}>
+          <div className={styles.ratings}>
+            <span>Ratings: </span>
+            {Array.from({ length: filledStars }, (_, i) => (
+              <Star key={i} className={styles.star} />
+            ))}
+            {Array.from({ length: emptyStars }, (_, i) => (
+              <Star key={`empty-${i}`} className={styles.emptyStar} />
+            ))}
+          </div>
+          <button
+            className={styles.bookButton}
+            onClick={handleBookAppointment}
+            aria-label="Book Appointment"
+          >
+            Book Appointment
+          </button>
+        </div>
       </div>
     </div>
   );
